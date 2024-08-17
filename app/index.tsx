40483a8fce9ea1311bbf0c44cc65e1b00e7f31bc
Guilherme Coutinho de Moura <69174628+CoutinhoGuilherme@users.noreply.github.com>
2024-08-17 04:06:44 +0000

    Aula 16-08-2024

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,47 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { Text, View, StyleSheet, Button, TouchableOpacity } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
+import { BorderlessButton } from "react-native-gesture-handler";
+import { useAnimatedKeyboard } from "react-native-reanimated";
+
+const produto = [
+  {id: 1, nome: "Coca-Cola", preco: 10.5},
+  {id: 1, nome: "Pepsi", preco: 9.5},
+  {id: 1, nome: "Fanta", preco: 8.5},
+  {id: 1, nome: "Dolly", preco: 6.5}
+]
+
+//const gradientColors = ['navy', 'blue', 'cyan'];
 
 export default function Index() {
+
+  let [contador, setContador] = useState(0);
+  let [fuga, setFuga] = useState(0);
+
   return (
     <View
+      //colors = {gradientColors}
       style = {styles.container}
     >
-      <Text style = {styles.text}>Hello World</Text>
+      {produto.map((p) => (
+        <View key = {p.id}>
+          <Text style = {styles.text}>{p.nome + " \u{1F964}"}</Text>
+          <Text style = {styles.textSmall}>{"R$ " + p.preco}</Text>
+        </View>
+      ))}
+
+      <TouchableOpacity style = {styles.buttonStyle}  onPress = {() => clicarBotao()}>
+        <Text style = {styles.buttonText}>Quantidade de Clicks {contador}</Text>
+      </TouchableOpacity>
+
+      {/* title = {"Quantidade de Clicks " + contador.toString()} */}
+
     </View>
   );
+
+  function clicarBotao() {
+    setContador(contador++);
+  }
 }
 
 const styles = StyleSheet.create({
@@ -15,11 +49,30 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "purple",
+    backgroundColor: "navy"
   },
   text: {
     color: "white",
     fontSize: 30,
+    fontWeight: "bold"
   },
+  textSmall: {
+    color: "black",
+    fontSize: 15,
+    fontWeight: "bold",
+    marginBottom: 5,
+    textAlign: "center"
+  },
+  buttonStyle: {
+    marginTop: 10,
+    backgroundColor: "black",
+    padding: 15,
+    borderRadius: 20
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 15,
+    fontWeight: "bold"
+  }
 });
 
